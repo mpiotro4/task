b30697441992d9cfa1f8d45d8c3736c1e074b670
mpiotro4 <mpiotro4@outlook.com>
2022-04-13 01:02:48 +0200

    csv

---
+++ src/app/CsvProducer.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+
+class CsvProducer
+{
+    static public function produceCSV($file_name, $arr)
+    {
+
+        $has_header = false;
+
+        $f = @fopen($file_name, "r+");
+        if ($f !== false) {
+            ftruncate($f, 0);
+            fclose($f);
+        }
+
+        foreach ($arr as $c) {
+            $fp = fopen($file_name, "a");
+
+            if (!$has_header) {
+                fputcsv($fp, array_keys($c));
+                $has_header = true;
+            }
+
+            fputcsv($fp, $c);
+            fclose($fp);
+        }
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 require(APP_PATH . 'EstoreScraper.php');
+require(APP_PATH . 'CsvProducer.php');
 
 $scraper = new EstoreScraper();
 
-echo '<pre>';
-print_r($scraper->getProductsWithPagination('http://estoremedia.space/DataIT/index.php?page=3'));
-echo '</pre>';
+$arr = $scraper->getProductsWithPagination('http://estoremedia.space/DataIT/index.php?page=3');
+
+CsvProducer::produceCSV("products.csv", $arr);
